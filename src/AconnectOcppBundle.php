d49ed297e3983c35134c8fa19f8a492ce39750c7
aconnect4881 <jlsuarez@proton.me>
2026-09-24 05:36:42 +0200

    Add src/AconnectOcppBundle.php for reusable Symfony bundle

---
+++ src/AconnectOcppBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Aconnect\OcppBundle;
+
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+/**
+ * Entry point for the reusable OCPP Symfony bundle.
+ */
+final class AconnectOcppBundle extends AbstractBundle
+{
+}
